3af630461b0f6b2ab60b67040522b5733316bd91
moritzws <moritzws@gmail.com>
2024-09-21 12:34:00 +0200

    Add json input for bot enpoint and update dependencies

--- start_fast_api.py
+++ start_fast_api.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from chatbot.chatbot import setup_vector_store, setup_embedding_model
 
+
+class TextInput(BaseModel):
+    input_text: str
+
+
 app = FastAPI()
+embedding_model = setup_embedding_model()
+vector_store = setup_vector_store(embedding_model)
 
 
-@app.get("/run-main")
-def run_main():
-    embedding_model = setup_embedding_model()
-    vector_store = setup_vector_store(embedding_model)
+# Set OpenAI API key to env variable OPENAI_API_KEY
 
-    query = "wer ist scrum master?"
-    results = vector_store.similarity_search(query, k=3)
-    return {"results": results}
+@app.get("/")
+async def root():
+    return {"status": "Server is up and running"}
+
+
+@app.post("/ask_bot/")
+async def process_text(input_query: TextInput):
+    result_text = vector_store.similarity_search(input_query.input_text, k=3)
+    return {"result_text": result_text}
 
 
 if __name__ == "__main__":
