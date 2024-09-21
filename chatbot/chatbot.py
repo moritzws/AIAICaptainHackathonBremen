03aa8bdc3b2a050048ed0f0ca2cd555ad77f4755
annkapopp <88618677+annkapopp@users.noreply.github.com>
2024-09-21 14:24:09 +0200

    update vector store functionality

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -8,20 +8,20 @@ from langchain.schema import Document
 import requests
 import os
 
+db_url = "https://gpt.hansehart.de/api/service"
+
 def setup_embedding_model():
-    model = "text-embedding-3-small"
+    model = "text-embedding-3-large"
     embedding_model = OpenAIEmbeddings(model=model)
     return embedding_model
 
 
-def setup_vector_store(embedding_model, db_token):
-
-    db_url = "https://gpt.hansehart.de/api/service/receive/persons"
+def setup_vector_store(embedding_model, db_url, db_token):
     database = requests.get(db_url, headers={"Authorization": f"Bearer {db_token}"}).json()
     documents = create_documents_from_db(database)
 
     """
-    path_to_xlsx = "/Users/annka/Downloads/KI-HackathonxROSSMANN_Challenge_Ansprechpartner-Chatbot.xlsx"
+    path_to_xlsx = "KI-HackathonxROSSMANN_Challenge_Ansprechpartner-Chatbot.xlsx"
     df = pd.read_excel(path_to_xlsx)
     documents = create_documents_from_df(df)
     """
@@ -31,20 +31,14 @@ def setup_vector_store(embedding_model, db_token):
     return vector_store
 
 
-def get_job_description_for_query(query, vector_store):
-    results = vector_store.similarity_search(query, k=3)
-    return results
-
-
 def get_personal_ids_for_query(query, vector_store):
-    documents = vector_store.similarity_search(query, k=3)
+    documents = vector_store.similarity_search(query.input_text, k=3)
     ids = [document.metadata.get("personal_id") for document in documents]
     return ids
 
 
 def create_documents_from_db(database):
     documents = []
-    personal_ids = []
     for employee in database:
         job_description = employee.get("beschreibung") if employee.get("beschreibung") else ""
         programs = employee.get("programme") if employee.get("programme") else ""
@@ -57,9 +51,9 @@ def create_documents_from_db(database):
         documents.append(description_document)
     return documents
 
+
 def create_documents_from_df(df):
     documents = []
-    personal_ids = []
     for index, row in df.iterrows():
         document = Document(page_content=row["Beschreibung der Position und Zuständigkeiten bei Problemen"], metadata={"personal_id": index})
         documents.append(document)
@@ -68,64 +62,82 @@ def create_documents_from_df(df):
     return documents
 
 
-def delete_employee_by_personal_id(personal_id, vector_store):
+def delete_employee_from_vector_store(personal_id, vector_store):
     document_ids = vector_store.get(where={"personal_id": personal_id}.get("ids"))
     vector_store.delete(ids=document_ids)
     return vector_store
 
 
+def add_employee_to_vector_store(personal_id, vector_store):
+    employee = requests.get(f"{db_url}/receive/person?id={personal_id}",
+                            headers={"Authorization": f"Bearer {os.environ['DB_TOKEN']}"}).json()
+
+    job_description = employee.get("beschreibung") if employee.get("beschreibung") else ""
+    programs = employee.get("programme") if employee.get("programme") else ""
+    personal_id = employee.get("id")
+
+    description_document = Document(page_content=job_description, metadata={"personal_id": personal_id})
+    job_document = Document(page_content=programs, metadata={"personal_id": personal_id})
+    vector_store.add_documents(documents=description_document)
+    vector_store.add_documents(documents=job_document)
+
+
 def update_employee_by_personal_id(personal_id, vector_store):
-    document_ids = vector_store.get(where={"personal_id": personal_id}.get("ids"))
-    documents = []
-    vector_store.update_documents(ids=document_ids, documents=documents)
+    delete_employee_from_vector_store(personal_id, vector_store)
+    add_employee_to_vector_store(personal_id, vector_store)
     return vector_store
 
 
 def get_summary_chain(llm):
     summary_template = """
-    Erstelle eine kurze Zusammenfassung, warum der Mitarbeiter bei der Anfrage helfen kann. Schreibe nur einen Satz, maximal zwei.
-    Gehe eher darauf ein, was der Mitarbeiter macht und von welchen Themen er Kenntnisse hat. Wiederhole nicht, auch nicht umschrieben, die Anfrage.
+    Erstelle eine kurze Zusammenfassung, warum der Mitarbeiter bei der Anfrage helfen kann. Schreibe nur einen Satz,
+    maximal zwei. Der Satz soll grammatisch korrekt und vollständig sein. Gehe eher darauf ein, was der Mitarbeiter
+    macht und von welchen Themen er Kenntnisse hat. Wiederhole nicht, auch nicht umschrieben, die Anfrage.
+    Nenne den Mitarbeiter nur beim Vornamen.
 
     Anfrage: {query}
-    Name: {name}
+    Vorname: {first_name}
+    Nachname: {last_name}
     Job: {job}
     Beschreibung des Jobs: {job_description}
 
     Diese Person kann helfen, weil:
     """
     summary_prompt = PromptTemplate(
-        input_variables=["query", "name", "job", "job_description"],
-        template=summary_template,
+        input_variables=["query", "first_name", "last_name", "job", "job_description"],
+        template=summary_template
     )
     summary_chain = LLMChain(llm=llm, prompt=summary_prompt)
     return summary_chain
 
 
-def get_summary(summary_chain, query, name, job, job_description):
-    summary = summary_chain.invoke(input={"query":query, "name":name, "job":job, "job_description":job_description})
+def get_summary(summary_chain, query, first_name, last_name, job, job_description):
+    summary = summary_chain.invoke(input={"query": query, "first_name": first_name, "last_name": last_name, "job": job,
+                                          "job_description": job_description})
     return summary
 
 
-def get_output_prompt():
+def get_output_prompt_for_one_employee():
     template = """
-    Du kannst dich an einen der folgenden Mitarbeiter wenden: \n
-    {name}, {job}\n{summary}
+    {first_name} {last_name}, {job}\n{summary}
     """
     output_prompt = PromptTemplate(
-        input_variables=["name", "job", "summary"],
+        input_variables=["first_name", "last_name", "job", "summary"],
         template=template,
     )
     return output_prompt
 
 
-def get_output(output_prompt, name, job, summary):
+def get_output(output_prompt, first_name, last_name, job, summary):
     # Benutze die generierte Zusammenfassung im Prompt
     output = output_prompt.format(
-        name=name,
+        first_name=first_name,
+        last_name=last_name,
         job=job,
         summary=summary["text"]
     )
     return output
+
 """
 def main():
     embedding_model = setup_embedding_model(openai_key)
--- start_fast_api.py
+++ start_fast_api.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from chatbot.chatbot import (setup_vector_store, setup_embedding_model, get_output_prompt, get_summary_chain,
-                             get_summary, get_output, get_personal_ids_for_query)
+from chatbot import (setup_vector_store, setup_embedding_model, get_output_prompt_for_one_employee,
+                             get_summary_chain, get_summary, get_output, get_personal_ids_for_query)
 import os
 from langchain_openai import OpenAI
-
+import requests
 
 class TextInput(BaseModel):
     input_text: str = "Wer kann mir beim Thema IT Security helfen?"
@@ -17,12 +17,14 @@ class VectorStoreUpdateInput(BaseModel):
 # Set OpenAI API key to env variable OPENAI_API_KEY
 # Set your database token to env variable DB_TOKEN="database_usage_token"
 
+db_url = "https://gpt.hansehart.de/api/service"
+
 app = FastAPI()
 llm = OpenAI()
 embedding_model = setup_embedding_model()
-vector_store = setup_vector_store(embedding_model, db_token=os.environ["DB_TOKEN"])
+vector_store = setup_vector_store(embedding_model, f"{db_url}/receive/persons", os.environ["DB_TOKEN"])
 summary_chain = get_summary_chain(llm=llm)
-output_prompt = get_output_prompt()
+output_prompt = get_output_prompt_for_one_employee()
 
 
 @app.get("/")
@@ -32,7 +34,7 @@ async def root():
 
 @app.post("/ask_bot/")
 async def process_text(input_query: TextInput):
-    result_text = vector_store.similarity_search(input_query.input_text, k=3)
+    result_text = process_query(input_query, vector_store, summary_chain, output_prompt)
     return {"result_text": result_text}
 
 
@@ -40,6 +42,7 @@ async def process_text(input_query: TextInput):
 async def update_vector_store(update_input: VectorStoreUpdateInput):
     try:
         print(update_input.id)
+
         # Trigger the update process (e.g., reload documents, re-index, etc.)
         pass
         return {"status": "Vector store update triggered successfully"}
@@ -47,12 +50,21 @@ async def update_vector_store(update_input: VectorStoreUpdateInput):
         return {"status": "Failed to trigger update", "error": str(e)}
 
 
-"""
 def process_query(query, vector_store, summary_chain, output_prompt):
+    outputs = []
     personal_ids = get_personal_ids_for_query(query, vector_store)
-    summary = get_summary(summary_chain, query, name, job, job_description)
-    output = get_output(output_prompt, name, job, summary)
-"""
+    for personal_id in personal_ids:
+        employee_data = requests.get(f"{db_url}/receive/person?id={personal_id}",
+                                     headers={"Authorization": f"Bearer {os.environ['DB_TOKEN']}"}).json()
+        summary = get_summary(summary_chain, query, employee_data.get("vorname"), employee_data.get("nachname"),
+                              employee_data.get("position"), employee_data.get("beschreibung"))
+        output = get_output(output_prompt, employee_data.get("vorname"), employee_data.get("nachname"),
+                            employee_data.get("position"), summary)
+        outputs.append(output)
+    final_output = ("Die folgenden Mitarbeiter können dir behilflich sein: \n"
+                    + "\n".join(outputs))
+    return final_output
+
 
 if __name__ == "__main__":
     import uvicorn
